9a48cb8869cc7a7ff30ebcf0a17ce5cd195ec948
Pranav Pasumarthy <53782063+TheRoadWarrior81@users.noreply.github.com>
2026-02-27 23:57:42 +0000

    add user registerer and login, routes, move secrets to .env

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
+from dotenv import load_dotenv
+load_dotenv()
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 import psycopg2
+import bcrypt
+import jwt
+import os
+from datetime import datetime, timedelta
+
 
 app  = FastAPI()
 
-DB_URL = "postgresql://neondb_owner:npg_eWMXFtv2b9Ai@ep-super-silence-aibswe2u-pooler.c-4.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+DB_URL = os.getenv("DB_URL")
+JWT_SECRET = os.getenv("JWT_SECRET")
 
 def get_db():
     conn = psycopg2.connect(DB_URL)
@@ -80,3 +89,72 @@ def get_circle(circle_id: int):
             "created_at": row[3]
         }
     return {"error": "Circle not found"}, 404
+
+class UserRegister(BaseModel):
+    username: str
+    email: str
+    password: str
+
+@app.post("/users/register")
+def register(user: UserRegister):
+    # Hash the password
+    password_hash = bcrypt.hashpw(
+        user.password.encode('utf-8'), 
+        bcrypt.gensalt()
+    ).decode('utf-8')
+    
+    conn = get_db()
+    cursor = conn.cursor()
+    
+    cursor.execute(
+        "INSERT INTO users (username, email, password_hash) VALUES (%s, %s, %s) RETURNING id, username, email;",
+        (user.username, user.email, password_hash)
+    )
+    
+    new_user = cursor.fetchone()
+    conn.commit()
+    conn.close()
+    
+    return {
+        "id": new_user[0],
+        "username": new_user[1],
+        "email": new_user[2]
+    }
+
+class UserLogin(BaseModel):
+    email: str
+    password: str
+
+@app.post("/users/login")
+def login(user: UserLogin):
+    conn  = get_db()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT id, username, email, password_hash FROM users WHERE email = %s;" , (user.email,))
+    row  = cursor.fetchone()
+    conn.close()
+
+    if not row:
+        return {"error": "User not found"}, 404
+    
+    # Check password against hash
+    password_matches = bcrypt.checkpw(
+        user.password.encode('utf-8'),
+        row[3].encode('utf-8')
+    )
+
+    if not password_matches:
+        return {"error": "Wrong password"}, 401
+    
+    #Creat JWT token
+    token = jwt.encode(
+        {
+        "user_id": row[0],
+        "username": row[1],
+        "exp": datetime.utcnow() + timedelta(days=7)
+        },
+        JWT_SECRET,
+        algorithm="HS256"
+    )
+
+    return {"token": token}
